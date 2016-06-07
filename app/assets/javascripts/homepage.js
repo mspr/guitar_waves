380dc259106b1ebff4a976df1a8e9c73b7085b4b
mspr <spriet.maxime.eisti@gmail.com>
2016-06-07 16:44:35 +0200

    Add Bootstrap and Angular dependencies through rails-assets system, deactivate turbolinks, change coffee extensions into js, add a simple angular example to check it works

---
+++ app/assets/javascripts/homepage.js
@@ -0,0 +1,8 @@
+var module = angular.module('ModuleName', []);
+
+module.controller('FirstController', ['$scope', FirstController]);
+
+function FirstController($scope)
+{
+  $scope.test = 'Hello world!';
+};
